7a4309c5df01812c74dfa0584a424ccf17a1eb55
Seok-Hee(Steve) Han <55567478+hse2527@users.noreply.github.com>
2026-09-16 02:41:40 -0500

    Add: is-anagram - submission-15

---
+++ Data Structures & Algorithms/is-anagram/submission-15.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        alph = [0] * 26
+
+        for i, ch in enumerate(s):
+            alph[ord(ch) - ord('a')] += 1
+            alph[ord(t[i]) - ord('a')] -= 1
+
+        for val in alph:
+            if val > 0:
+                return False
+        
+        return True
+
