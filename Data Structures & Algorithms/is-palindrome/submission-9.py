04875f44ae501fece9b5b34c1ec826a7e48fc76b
Seok-Hee(Steve) Han <55567478+hse2527@users.noreply.github.com>
2026-09-18 21:36:11 -0500

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.py
@@ -0,0 +1,16 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = re.sub(r'[^A-Za-z0-9]', '', s).lower()
+    
+        i, j = 0, len(cleaned) - 1
+
+        while i <= j:
+            if cleaned[i] != cleaned[j]:
+                return False
+            i += 1
+            j -= 1
+
+        return True
+            
