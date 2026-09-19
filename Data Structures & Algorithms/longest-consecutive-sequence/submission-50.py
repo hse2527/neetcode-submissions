595e4fca61afd749f51dac463990a3d24e1eb6ab
Seok-Hee(Steve) Han <55567478+hse2527@users.noreply.github.com>
2026-09-18 21:27:10 -0500

    Add: longest-consecutive-sequence - submission-50

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-50.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nset = set()
+
+        for num in nums:
+            nset.add(num)
+
+        high = 0
+        for num in nums:
+            if num-1 not in nset:
+                l = 0
+                while num in nset:
+                    num += 1
+                    l += 1
+                if l > high:
+                    high = l
+        
+        return high
+
